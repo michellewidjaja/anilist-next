75f475aa22b4829a7fc9607a120ba87cc0bc15e3
michellewidjaja <michelle.widjaja@gmail.com>
2025-05-24 21:52:50 +0700

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+// jest.config.ts
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/lib/(.*)$': '<rootDir>/lib/$1',
+  },
+};
